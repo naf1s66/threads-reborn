e4b59f5da0e9e7a5f4c83a8be7d7394547580964
naf1s66 <160537802+naf1s66@users.noreply.github.com>
2024-09-18 11:05:56 +0600

    Update register.php

--- register.php
+++ register.php
@@ -69,7 +69,7 @@ $conn->close();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"> 
     <title>Document</title>
-    <?php include('register.php');?>
+    
 
     
 </head>
